3a9de8502e16b33ded0cc2f7302929c5c876294d
jltxpro <jltxpro@gmail.com>
2021-01-26 17:00:29 -0600

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace WinForm_ConnSQLStoredProcResultsDisp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSSL = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDatabase = new System.Windows.Forms.TextBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtStoredProcedure = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(94, 22);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(160, 23);
+            this.txtServer.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.txtStoredProcedure);
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtSSL);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtPort);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.txtUser);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtDatabase);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtServer);
+            this.groupBox1.Location = new System.Drawing.Point(9, 17);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(409, 237);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "MySQL Server Connection Settings";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(300, 194);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(91, 28);
+            this.btnConnect.TabIndex = 7;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(150, 141);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(82, 15);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "(default: 3306)";
+            this.label8.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(150, 170);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 15);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "(default: none)";
+            this.label7.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 170);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 15);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "SSL :";
+            this.label6.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtSSL
+            // 
+            this.txtSSL.Location = new System.Drawing.Point(94, 167);
+            this.txtSSL.Name = "txtSSL";
+            this.txtSSL.Size = new System.Drawing.Size(50, 23);
+            this.txtSSL.TabIndex = 6;
+            this.txtSSL.Text = "none";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 15);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Port :";
+            this.label5.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(94, 138);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(50, 23);
+            this.txtPort.TabIndex = 5;
+            this.txtPort.Text = "3306";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 15);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Password :";
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(94, 109);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(103, 23);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(94, 80);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(103, 23);
+            this.txtUser.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "User :";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Database :";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtDatabase
+            // 
+            this.txtDatabase.Location = new System.Drawing.Point(94, 51);
+            this.txtDatabase.Name = "txtDatabase";
+            this.txtDatabase.Size = new System.Drawing.Size(160, 23);
+            this.txtDatabase.TabIndex = 2;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.ForeColor = System.Drawing.Color.Red;
+            this.lblMessage.Location = new System.Drawing.Point(11, 394);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(16, 15);
+            this.lblMessage.TabIndex = 3;
+            this.lblMessage.Text = "...";
+            // 
+            // txtStoredProcedure
+            // 
+            this.txtStoredProcedure.Location = new System.Drawing.Point(94, 196);
+            this.txtStoredProcedure.Name = "txtStoredProcedure";
+            this.txtStoredProcedure.Size = new System.Drawing.Size(160, 23);
+            this.txtStoredProcedure.TabIndex = 1;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(13, 199);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 15);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Stored Proc. :";
+            this.label9.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 421);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDatabase;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSSL;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtStoredProcedure;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,207 @@
+﻿using CobraCore.Utilities;
+using Entities.Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm_ConnSQLStoredProcResultsDisp
+{
+    public partial class Form1 : Form
+    {
+        //constructor 
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.lblMessage.Visible = false;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            this.lblMessage.Visible = false;
+
+            // Verify that the fields are  not empty 
+            if (string.IsNullOrWhiteSpace(this.txtServer.Text) ||
+                string.IsNullOrWhiteSpace(this.txtDatabase.Text) ||
+                string.IsNullOrWhiteSpace(this.txtUser.Text) ||
+                string.IsNullOrWhiteSpace(this.txtPassword.Text) ||
+                string.IsNullOrWhiteSpace(this.txtPort.Text) ||
+                string.IsNullOrWhiteSpace(this.txtSSL.Text) ||
+                string.IsNullOrWhiteSpace(this.txtStoredProcedure.Text))
+            {
+                this.lblMessage.ForeColor = Color.Red;
+                this.lblMessage.Text = "Error: Please complete all SQL Server Connection Settings.";
+                this.lblMessage.Visible = true;
+
+                return;
+            }
+
+            string connString = String.Format("server={0};port={1};user id={2}; password={3}; database={4}; SslMode={5}",
+                    this.txtServer.Text, this.txtPort.Text, this.txtUser.Text, this.txtPassword.Text, this.txtDatabase.Text, this.txtSSL.Text);
+
+            ConnectionFactory cF = new ConnectionFactory();
+            MySqlConnection connection = cF.CreateConnection(connString);
+            StoredProcedureSchema spSchema = new StoredProcedureSchema();
+
+            var result = GetAll(connection, spSchema, this.txtStoredProcedure.Text);
+
+            /*
+            try
+            {
+                connection.Open();
+                IDbCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = this.txtStoredProcedure.Text; //obj.GetCommandText_GetAll();
+
+                // Let's run the stored proc to get the column names, 
+                //DataTable table = connection.GetSchema("Procedures");
+                //DisplayData(table);
+
+                //DbParameterService.AddWithValue(command, "_subscriberid", subscriberId);
+
+                using (var reader = command.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    bool hasRows = false;
+
+                    while (reader.Read())
+                    {
+                        if (!hasRows)
+                            hasRows = true;
+                    }
+
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                this.lblMessage.ForeColor = Color.Red;
+                this.lblMessage.Text = $"Error: {ex.Message}";
+                this.lblMessage.Visible = true;
+                //throw new Exception(ex.Message);
+            }
+            finally
+            {
+                if (connection is IDisposable)
+                {
+                    var disposableConnection = connection as IDisposable;
+                    disposableConnection.Dispose();
+                }
+            }
+            */
+
+
+            // if connection tested OK, then proceed with running stored proc
+            //var companies = new Interactor<Company>(new ConnectionFactory()).GetAll(subscriberId, new Company());
+            //var result = _mapper.Map<IEnumerable<CompanyDto>>(companies);
+
+            //
+
+        }
+
+
+
+        public IEnumerable<StoredProcedureSchema> GetAll(MySqlConnection connection, 
+            StoredProcedureSchema obj, string storedProcedureName)
+        {
+            var objList = new List<StoredProcedureSchema>();
+
+            try
+            {
+                connection.Open();
+                IDbCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = storedProcedureName;
+
+                using (var reader = command.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    bool hasRows = false;
+
+                    while (reader.Read())
+                    {
+                        var tmpObj = Activator.CreateInstance(typeof(StoredProcedureSchema));
+
+                        foreach (var item in obj.GetFieldMapper())
+                        {
+                            var propertyName = item.Key;
+                            var propertyInfo = tmpObj.GetType().GetProperty(propertyName);
+
+                            // make sure object has the property 
+                            if (propertyInfo != null)
+                            {
+                                string dbField = item.Value;
+                                if (propertyInfo.PropertyType == typeof(System.DateTime) ||
+                                            propertyInfo.PropertyType == typeof(System.Int32))
+                                    propertyInfo.SetValue(tmpObj, reader[dbField]);
+                                else
+                                    propertyInfo.SetValue(tmpObj, reader[dbField].ToString());
+                            }
+                        }
+
+                        objList.Add((StoredProcedureSchema)tmpObj);
+
+                        if (!hasRows)
+                            hasRows = true;
+                    }
+
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                // throw new Exception(ex.Message);
+                this.lblMessage.ForeColor = Color.Red;
+                this.lblMessage.Text = $"Error: {ex.Message}";
+                this.lblMessage.Visible = true;
+            }
+            finally
+            {
+                if (connection is IDisposable)
+                {
+                    var disposableConnection = connection as IDisposable;
+                    disposableConnection.Dispose();
+                }
+            }
+
+            return objList;
+        }
+
+
+
+
+
+
+
+
+        private static void DisplayData(System.Data.DataTable table)
+        {
+            foreach (System.Data.DataRow row in table.Rows)
+            {
+                foreach (System.Data.DataColumn col in table.Columns)
+                {
+                    Debug.WriteLine("{0} = {1}", col.ColumnName, row[col]);
+                }
+                Debug.WriteLine("============================");
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
